00d7f25512b102e5152e3e93607eb3310e0ade68
Olavur Ellefsen <olavur@Olavurs-MacBook-Pro.local>
2018-11-22 09:42:58 +0000

    Change name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            MicroserviceShop
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            The ultimate shopping experience
           </a>
         </header>
       </div>
