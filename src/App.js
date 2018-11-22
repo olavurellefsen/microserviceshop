fb36ca05aec6eb798d44cfeee40680b91b8538ea
Olavur Ellefsen <olavur@Olavurs-MacBook-Pro.local>
2018-11-22 10:13:14 +0000

    Setup routing and simple products page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            MicroserviceShop
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            The ultimate shopping experience
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/products" render={() => 'Products'} />
+          <Route exact path="/" render={() =>
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  MicroserviceShop
+                </p>
+                <a
+                  className="App-link"
+                  href="/products"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  The ultimate shopping experience
+                </a>
+              </header>
+            </div>
+          }
+        />
+        </Switch>
+      </Router>
     );
   }
 }
