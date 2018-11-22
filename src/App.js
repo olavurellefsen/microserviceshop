cf20457f48a647d9ccb8db4a28d955a2b492f9b3
Sveinbjørn Jakobsen <sjak@ft.fo>
2018-11-22 19:13:51 +0530

    added login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Products from './Products/Products'
+import Products from './Products/Products';
+import Login from './Login/Login.js';
+
+
 import './App.css';
 
 class App extends Component {
@@ -9,6 +12,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route path="/products" render={() => <Products />} />
+          <Route path="/login" render={() => <Login />} />
           <Route exact path="/" render={() =>
             <div className="App">
               <header className="App-header">
@@ -20,6 +24,12 @@ class App extends Component {
                   href="/products"
                 >
                   The ultimate shopping experience
+                </a>
+                <a
+                  className="App-link"
+                  href="/login"
+                >
+                  login
                 </a>
               </header>
             </div>
